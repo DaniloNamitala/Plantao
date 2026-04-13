f2f4a1df2e9934b0abb85e80b378182b6a6e3cce
Danilo Namitala <danilonamitala@yahoo.com.br>
2026-04-12 21:45:21 -0300

    funcional nativo

---
+++ data/database.ts
@@ -0,0 +1,17 @@
+import * as SQLite from "expo-sqlite";
+import { runMigrations } from "./migrations";
+
+const DB_NAME = "plantao.db";
+
+let dbPromise: Promise<SQLite.SQLiteDatabase> | null = null;
+
+export function getDatabase(): Promise<SQLite.SQLiteDatabase> {
+  if (!dbPromise) {
+    dbPromise = (async () => {
+      const db = await SQLite.openDatabaseAsync(DB_NAME);
+      await runMigrations(db);
+      return db;
+    })();
+  }
+  return dbPromise;
+}
